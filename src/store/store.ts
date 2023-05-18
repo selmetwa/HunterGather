d5f0d1197b60045e739339b3055a96f5c024f586
Sherif Elmetwally <sge5@njit.edu>
2023-05-18 10:57:34 -0400

    some comments

--- src/store/store.ts
+++ src/store/store.ts
@@ -3,9 +3,12 @@ import { writable } from 'svelte/store';
 export const modalStore = writable(false);  
 export const collectionIds = writable([]);
 export const myBlocks = writable([]);
+
+// collecting
 export const collectingModal = writable(false);
 export const objectToCollect = writable({});
 
+// preview
 export const previewPanel = writable(false);
 export const previewPanelObject = writable({ type: '', object: {} });
 
