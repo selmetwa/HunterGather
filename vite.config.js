ae3a4f09b89082239df64c4fdf0ff12d11d783bf
Sherif Elmetwally <sge5@njit.edu>
2023-05-24 00:44:42 -0400

    shrug

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,17 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+  plugins: [sveltekit()],
+  build: {
+    rollupOptions: {
+      external: [
+        "@supabase/gotrue-js", // ignore react stuff
+      ],
+    },
+  },
+  test: {
+    include: ['src/**/*.{test,spec}.{js,ts}']
+  }
 };
 
 export default config;
