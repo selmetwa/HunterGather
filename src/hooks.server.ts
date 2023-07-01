b93411c5092ddf0ee2134bb23b1688c04444bd53
Sherif Elmetwally <sge5@njit.edu>
2023-07-01 19:52:15 -0400

    shrug

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -8,5 +8,25 @@ export const handle: Handle = async ({ event, resolve }) => {
 	event.locals.sb = supabaseClient;
 	event.locals.session = session;
 
-	return resolve(event);
+	// Apply CORS header for API routes
+	if (event.url.pathname.startsWith('/api')) {
+		// Required for CORS to work
+		if (event.request.method === 'OPTIONS') {
+			return new Response(null, {
+				headers: {
+					'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+					'Access-Control-Allow-Origin': '*',
+					'Access-Control-Allow-Headers': '*'
+				}
+			});
+		}
+	}
+
+	const response = await resolve(event);
+	if (event.url.pathname.startsWith('/api')) {
+		response.headers.append('Access-Control-Allow-Origin', `*`);
+	}
+	return response;
+
+	// return resolve(evenxt);
 };
