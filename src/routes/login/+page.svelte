687d6035c624389b3610febdd2fe816547d1968a
Sherif Elmetwally <sge5@njit.edu>
2023-07-04 11:44:42 -0400

    add social auth

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -40,6 +40,25 @@
 					>not a member? <span class="font-semibold text-action-400">Join</span></a
 				>
 			</div>
+			<div class="relative flex py-4 items-center">
+				<div class="flex-grow border-t border-gray-200" />
+				<span class="flex-shrink font-light mx-4 text-gray-400">or continue with</span>
+				<div class="flex-grow border-t border-gray-200" />
+			</div>
+			<form class="socials flex flex-row gap-4 h-12" method="POST">
+				<button
+					formaction="?/login&provider=github"
+					class="border border-gray-300 basis-1/2 bg-slate-50 rounded hover:bg-slate-100 ease-in-out duration-300 drop-shadow-sm"
+				>
+					<i class="fa-brands fa-github fa-2xl" />
+				</button>
+				<button
+					formaction="?/login&provider=google"
+					class="border border-gray-300 basis-1/2 bg-slate-50 rounded hover:bg-slate-100 ease-in-out duration-300 drop-shadow-sm"
+				>
+					<i class="fa-brands fa-google fa-2xl" />
+				</button>
+			</form>
 		</main>
 	</section>
 </body>
