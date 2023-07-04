2eb617ad81a93e1297303b2168983dbc642d2703
Sherif Elmetwally <sge5@njit.edu>
2023-07-04 00:19:01 -0400

    make about

---
+++ src/routes/privacy/+page.svelte
@@ -0,0 +1,3 @@
+<h1>Privacy Policy</h1>
+<p>I don't collect and user data other than emails and thats for auth, all payments are conducted via stripe, you can view their privacy policy here https://stripe.com/privacy</p>
+<p>Feel free to email me at selmetwa@gmail.com if you have any questions</p>
