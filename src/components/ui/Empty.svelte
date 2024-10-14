4902b1ead7edb9a087f807fba126cc9d706483e5
Sherif Elmetwally <sge5@njit.edu>
2024-10-13 22:50:51 -0400

    remove learn thing

--- src/components/ui/Empty.svelte
+++ src/components/ui/Empty.svelte
@@ -1,23 +1,3 @@
-<script>
-  let learnNewWord = false
-</script>
 <div class="flex flex-col items-center border-2 border-gray-300 background-gray-200 text-center p-20 m-20">
-  <p>
-    {#if learnNewWord}
-      <p class="text-gray-400 m-auto">
-        <a class="text-blue-400" href="https://en.wiktionary.org/wiki/mafeesh" target="_blank" rel="noreferrer">
-          مـَفيش (māfeesh)
-        </a>
-        = there is nothing</p>
-    {:else}
-      <p class="text-gray-400 m-auto">there is nothing</p>
-    {/if}
-  </p>
-  {#if learnNewWord}
-  <button class="mt-4 text-gray-500 font-light underline" on:click={() => learnNewWord = false}>back to engish please</button>
-
-  {:else}
-  <button class="mt-4 text-gray-500 font-light underline" on:click={() => learnNewWord = true}>teach me something</button>
-
-  {/if}
+  <p class="text-gray-400 m-auto">there is nothing</p>
 </div>
